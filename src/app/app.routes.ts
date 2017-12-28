4283ac97d3e76541e993a54a028c6fe1710e77d7
Cat Yanish <catyanish@gmail.com>
2017-12-27 23:19:25 -0600

    imported peoplelist component and created default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,1 +1,19 @@
 import { Routes } from '@angular/router';
+import { PeopleListComponent } from './people-list/people-list.component';
+
+
+
+
+const routes: Routes = [
+  // map '/persons' to the people list component
+  {
+    path: 'persons',
+    component: PeopleListComponent,
+  },
+  // map '/' to '/persons' as our default route
+  {
+    path: '',
+    redirectTo: '/persons',
+    pathMatch: 'full'
+  },
+];
